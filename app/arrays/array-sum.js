8296e3595aadea63078c794e93083366adbd46e2
johnmendez <john.a.mendez2012@gmail.com>
2017-02-20 08:55:41 -0600

    Finished for now

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,1 +1,10 @@
-export default function () {}
+export default function arraySum(arr) {
+  const length = arr.length;
+  let arraySumValue = 0;
+
+  for (let i = 0; i < length; i++) {
+    arraySumValue += arr[i];
+  }
+
+  return arraySumValue;
+}
--- app/conway/should-revive.js
+++ app/conway/should-revive.js
@@ -1,3 +1,11 @@
 import aliveNeighbors from './alive-neighbors';
 
-export default function () {}
+export default function reviveEh(value) {
+  const aliveSum = aliveNeighbors(value);
+
+  if (aliveSum === 3) {
+    return true;
+  }
+
+  return false;
+}
